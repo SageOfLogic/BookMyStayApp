1815947e7d57cc86fcf742ca267edcf95d4f872c
parnavvii <parnavisharma311@gmail.com>
2026-02-27 14:40:34 +0530

    Welcome Message

---
+++ Documents/RA2412707010015/App/src/PalindromeApp.java
@@ -0,0 +1,8 @@
+public class PalindromeApp {
+    static void main() {
+        System.out.println("Welcome to Palindrome Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System intialized successfully.");
+
+    }
+}
