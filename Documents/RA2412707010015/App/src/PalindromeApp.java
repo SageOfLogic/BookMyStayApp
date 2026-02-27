986ab2e47eb0891a21e103cb9b5ec108397a4679
parnavvii <parnavisharma311@gmail.com>
2026-02-27 16:01:36 +0530

    uc2 modified Message2

--- Documents/RA2412707010015/App/src/PalindromeApp.java
+++ Documents/RA2412707010015/App/src/PalindromeApp.java
@@ -1,9 +1,26 @@
 import java.util.SortedMap;
+import java.util.Scanner;
 
 public class PalindromeApp {
-    static void main() {
 
-        System.out.println(" usecase1");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        String word = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Decision: It is  a Palindrome.");
+        } else {
+            System.out.println("Decision: It is NOT a Palindrome.");
+        }
 
     }
 }
